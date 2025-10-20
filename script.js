e3091314e47f586c85336beb77d0eadaaf8dd9b6
vishalkashyap1155 <vishalkashyap1155@gmail.com>
2025-10-20 19:32:57 +0530

    files are modified

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+const navLinks = document.querySelectorAll("header nav a");
+const logoLink = document.querySelector(".logo");
+
+
+const activePage = ()=>{
+    navLinks.forEach(link =>{
+        link.classList.remove("active")
+    })
+}
+
+navLinks.forEach((link,idx)=>{
+    link.addEventListener("click", ()=>{
+        if(!link.classList.contains("active")){
+            activePage();
+
+            link.classList.add("active");
+        }
+    });
+});
+
+logoLink.addEventListener("click", ()=>{
+    if(!navLinks[0].classList.contains("active")){
+        activePage();
+        navLinks[0].classList.add("active");
+    }
+})
+
 let resumeBtns = document.querySelectorAll(".resume-btn");
 
 resumeBtns.forEach((btn, idx) =>{
@@ -18,3 +45,54 @@ resumeBtns.forEach((btn, idx) =>{
 
     })
 })
+
+
+let arrowRight = document.querySelector(".protfolio-box .navigation .arrow-right");
+let arrowLeft = document.querySelector(".protfolio-box .navigation .arrow-left");
+
+let index = 0;
+
+const activePortfolio = () =>{
+    const imgSlide = document.querySelector(".portfolio-carousel .image-slide");
+
+    const portfolioDetails = document.querySelectorAll(".protfolio-detail");
+   
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+
+    portfolioDetails.forEach(detail =>{
+        detail.classList.remove("active");
+    });
+
+    portfolioDetails[index].classList.add("active");
+} 
+
+
+
+arrowRight.addEventListener("click", ()=>{
+    if(index < 4){
+        index++;
+        arrowLeft.classList.remove("disabled")
+    }
+
+    else{
+        index = 5;
+        arrowRight.classList.add("disabled")
+    }
+    activePortfolio();
+});
+
+
+
+arrowLeft.addEventListener("click", ()=>{
+    if(index > 1){
+        index--;
+        arrowRight.classList.remove("disabled")
+    }
+
+    else{
+        index = 0;
+        arrowLeft.classList.add("disabled")
+    }
+    activePortfolio();
+});
